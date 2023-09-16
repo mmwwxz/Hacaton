d04b104987bd5f1d82d351eff4b77697ec6e0f35
janelaxag <hellhole.gc@gmail.com>
2023-09-16 18:11:58 +0600

    Update Header

--- src/components/organism/HeaderOrganism/HeaderOrganism.tsx
+++ src/components/organism/HeaderOrganism/HeaderOrganism.tsx
@@ -12,39 +12,40 @@ export const HeaderOrganism: FC = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <>
-      <div className='shadow-md w-full fixed top-0 left-0'>
-        <div className='md:flex items-center justify-between bg-black text-white py-4 md:px-10 px-7'>
-          <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
-            <img src={logo} alt='Logo Earth' />
-            <span className='font-amita'>Save Earth</span>
-          </div>
-          {/* Menu icon */}
-          <div
-            onClick={() => setOpen(!open)}
-            className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7'
-          >
-            {open ? <XMarkIcon /> : <Bars3BottomRightIcon />}
-          </div>
-          {/* link items */}
-          <ul
-            className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-black text-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-              open ? 'top-12' : 'top-[-490px]'
-            }`}
-          >
-            {Links.map((link) => (
-              <li className='md:ml-8 md:my-0 my-7 font-semibold'>
-                <a
-                  href={link.link}
-                  className='text-gray-200 hover:text-blue-400 duration-500'
-                >
-                  {link.name}
-                </a>
-              </li>
-            ))}
-          </ul>
+    <div className='shadow-md w-full fixed top-0 left-0'>
+      <div className='md:flex items-center justify-between bg-black text-white py-4 md:px-10 px-7'>
+        {/* Logo */}
+        <div className='font-bold text-2xl cursor-pointer flex items-center gap-2'>
+          <img src={logo} alt='Logo Earth' className='h-8 w-8' />{' '}
+          <span className='font-amita text-xl'>Save Earth</span>{' '}
         </div>
+        <div
+          onClick={() => setOpen(!open)}
+          className='md:hidden flex items-center cursor-pointer'
+        >
+          {open ? (
+            <XMarkIcon className='w-6 h-6' />
+          ) : (
+            <Bars3BottomRightIcon className='w-6 h-6' />
+          )}
+        </div>
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-black text-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? 'top-12' : '-top-[490px]'
+          }`}
+        >
+          {Links.map((link) => (
+            <li key={link.name} className='md:ml-6 md:my-0 my-4 font-semibold'>
+              <a
+                href={link.link}
+                className='text-gray-200 hover:text-blue-400 duration-500'
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
-    </>
+    </div>
   );
 };
