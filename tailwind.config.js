d64dae125adad5d80443266d194756a2c9d6d925
janelaxag <hellhole.gc@gmail.com>
2023-09-16 16:38:26 +0600

    add fonts and jsx tsx content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        amita: ['Amatic SC', 'cursive'],
+      },
+    },
   },
   plugins: [],
-}
+};
